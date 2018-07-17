48be546f639a0902b1d1689d9ae0237fb5b52895
Antagonize <32521670+Antagonize@users.noreply.github.com>
2018-07-17 14:09:37 -0700

    a circle
    its a circle

--- my-game.js
+++ my-game.js
@@ -1,71 +1,36 @@
-// constants to set up the environment
+float x = width/2;
+float y = height/2;
+float x_dir = 10;
 
-const CANVAS_WIDTH = 500;
+void setup ()  {
+size (700, 700); 
 
-const CANVAS_HEIGHT = 500;
-
-const BACKGROUND_COLOR = 'lightblue';
-
-
-
-// location of the player
-
-var playerX = 100;
-
-var playerY = 50;
-
-var name = "sam";
-
-function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  //fill('grey');
-  //arc(200, 50, 100, 80, 0, PI, CHORD);
 }
 
-// loops forever
-function draw() {
-  // Draw background
-  background(BACKGROUND_COLOR);
-  
-  // Draw player
-  drawPlayer(playerX, playerY);
-  
-  // Player movement
-  if (keyIsDown(LEFT_ARROW)) {
-    playerX--;
-  }
+//drawing the circle
 
-  else if (keyIsDown(RIGHT_ARROW)) {
-    playerX++;
-  }
+void draw () {
+  background (124); 
+  frameRate (24); 
+  strokeWeight (5);
+  fill (255); 
+ ellipse (x, y, 100, 100); 
+ 
+ 
+   x = x + x_dir;
+    
 
-  else if (keyIsDown(UP_ARROW)) {
-    playerY--;
+  if (x >= width) {
+    x_dir = -1 * x_dir;
   }
+  else if (x <= 0) {
+    }
 
-  else if (keyIsDown(DOWN_ARROW)) {
-    playerY++;
-  }
 }
 
-// draws a snowman
-function drawPlayer(pX, pY) {
-  //replace this...
-  
-	noStroke();
-  fill('white');
-  
-  // body
-  ellipse(pX, pY, 50, 50);
-  ellipse(pX, pY+50, 75, 75);
-  ellipse(pX, pY+100, 100, 100);
-  
-  // eyes
-  fill('black');
-  ellipse(pX-10, pY-5, 10, 10);
-  ellipse(pX+10, pY-5, 10, 10);
-  
-  // nose
-  fill('orange');
-  triangle(pX, pY, pX+10, pY+5, pX-10, pY+10);
+void mousePressed () {
+
 }
+
+  
+ 
